bc549a78f0e49645dea72187b44d3ab7e4340a25
Martin Myslik <martin.myslik@ataccama.com>
2019-05-15 12:15:22 +0200

    task/#82 Add tests for quest service

--- src/test/java/com/withergate/api/service/quest/QuestServiceTest.java
+++ src/test/java/com/withergate/api/service/quest/QuestServiceTest.java
@@ -1,8 +1,11 @@
 package com.withergate.api.service.quest;
 
 import com.withergate.api.model.Clan;
+import com.withergate.api.model.action.ActionState;
 import com.withergate.api.model.action.QuestAction;
 import com.withergate.api.model.character.Character;
+import com.withergate.api.model.character.CharacterState;
+import com.withergate.api.model.encounter.Encounter;
 import com.withergate.api.model.notification.ClanNotification;
 import com.withergate.api.model.quest.Quest;
 import com.withergate.api.model.quest.QuestDetails;
@@ -10,6 +13,7 @@ import com.withergate.api.model.quest.QuestDetails.Type;
 import com.withergate.api.repository.action.QuestActionRepository;
 import com.withergate.api.repository.quest.QuestDetailsRepository;
 import com.withergate.api.service.RandomService;
+import com.withergate.api.service.RandomServiceImpl;
 import com.withergate.api.service.encounter.CombatService;
 import com.withergate.api.service.notification.NotificationService;
 
@@ -90,6 +94,139 @@ public class QuestServiceTest {
 
         // then verify action saved
         Mockito.verify(questActionRepository).save(action);
+    }
+
+    @Test
+    public void testGivenQuestActionWhenSuccessThenVerifyEntitiesUpdated() {
+        // given quest action
+        Character character = new Character();
+        character.setId(1);
+        character.setIntellect(5);
+        character.setExperience(0);
+        character.setState(CharacterState.BUSY);
+
+        Clan clan = new Clan();
+        clan.setId(1);
+        character.setClan(clan);
+
+        QuestDetails details = new QuestDetails();
+        details.setType(Type.INTELLECT);
+        details.setDifficulty(1);
+        details.setCompletion(4);
+
+        Quest quest = new Quest();
+        quest.setCompleted(false);
+        quest.setProgress(0);
+        quest.setDetails(details);
+        quest.setClan(clan);
+
+        QuestAction action = new QuestAction();
+        action.setQuest(quest);
+        action.setCharacter(character);
+        action.setState(ActionState.PENDING);
+        List<QuestAction> actions = new ArrayList<>();
+        actions.add(action);
+        Mockito.when(questActionRepository.findAllByState(ActionState.PENDING)).thenReturn(actions);
+
+        // when succeeding in quest action
+        Mockito.when(randomService.getRandomInt(1, RandomServiceImpl.K6)).thenReturn(6); // high roll
+
+        questService.processQuestActions(1);
+
+        // then verify entities updated
+        Assert.assertEquals(1, quest.getProgress());
+        Assert.assertEquals(2, character.getExperience());
+        Assert.assertEquals(ActionState.COMPLETED, action.getState());
+    }
+
+    @Test
+    public void testGivenQuestActionWhenSuccessInProgressedQuestThenVerifyEntitiesUpdated() {
+        // given quest action
+        Character character = new Character();
+        character.setId(1);
+        character.setCraftsmanship(5);
+        character.setExperience(0);
+        character.setState(CharacterState.BUSY);
+
+        Clan clan = new Clan();
+        clan.setId(1);
+        clan.setFame(10);
+        clan.setCaps(10);
+        character.setClan(clan);
+
+        QuestDetails details = new QuestDetails();
+        details.setType(Type.CRAFTSMANSHIP);
+        details.setDifficulty(1);
+        details.setCompletion(4);
+        details.setFameReward(20);
+        details.setCapsReward(10);
+
+        Quest quest = new Quest();
+        quest.setCompleted(false);
+        quest.setProgress(3);
+        quest.setDetails(details);
+        quest.setClan(clan);
+
+        QuestAction action = new QuestAction();
+        action.setQuest(quest);
+        action.setCharacter(character);
+        action.setState(ActionState.PENDING);
+        List<QuestAction> actions = new ArrayList<>();
+        actions.add(action);
+        Mockito.when(questActionRepository.findAllByState(ActionState.PENDING)).thenReturn(actions);
+
+        // when succeeding in quest action
+        Mockito.when(randomService.getRandomInt(1, RandomServiceImpl.K6)).thenReturn(6); // high roll
+
+        questService.processQuestActions(1);
+
+        // then verify entities updated
+        Assert.assertTrue(quest.isCompleted());
+        Assert.assertEquals(2, character.getExperience());
+        Assert.assertEquals(30, clan.getFame());
+        Assert.assertEquals(20, clan.getCaps());
+        Assert.assertEquals(ActionState.COMPLETED, action.getState());
+    }
+
+    @Test
+    public void testGivenQuestActionWhenHandlingCombatThenVerifyCombatServiceCalled() {
+        // given quest action
+        Character character = new Character();
+        character.setId(1);
+        character.setState(CharacterState.BUSY);
+
+        Clan clan = new Clan();
+        clan.setId(1);
+        clan.setFame(10);
+        clan.setCaps(10);
+        character.setClan(clan);
+
+        QuestDetails details = new QuestDetails();
+        details.setType(Type.COMBAT);
+        details.setDifficulty(1);
+        details.setCompletion(4);
+
+        Quest quest = new Quest();
+        quest.setCompleted(false);
+        quest.setProgress(0);
+        quest.setDetails(details);
+        quest.setClan(clan);
+
+        QuestAction action = new QuestAction();
+        action.setQuest(quest);
+        action.setCharacter(character);
+        action.setState(ActionState.PENDING);
+        List<QuestAction> actions = new ArrayList<>();
+        actions.add(action);
+        Mockito.when(questActionRepository.findAllByState(ActionState.PENDING)).thenReturn(actions);
+
+        // when handling actions
+        questService.processQuestActions(1);
+
+        // then verify combat service called
+        Mockito.verify(combatService).handleSingleCombat(Mockito.any(ClanNotification.class), Mockito.any(Encounter.class),
+                Mockito.eq(character));
+        Assert.assertEquals(ActionState.COMPLETED, action.getState());
     }
 
 }
