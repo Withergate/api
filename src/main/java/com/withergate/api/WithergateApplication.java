30af8cc848fc9277d41df3c2e803262dc0530ead
Martin Myslik <martin.myslik@ataccama.com>
2019-07-04 11:02:14 +0200

    task/#99 Inject cors origin from properties

--- src/main/java/com/withergate/api/WithergateApplication.java
+++ src/main/java/com/withergate/api/WithergateApplication.java
@@ -6,11 +6,7 @@ import javax.annotation.PostConstruct;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
  * Main application class.
@@ -28,21 +24,6 @@ public class WithergateApplication {
      */
     public static void main(String[] args) {
         SpringApplication.run(WithergateApplication.class, args);
-    }
-
-    /**
-     * CORS configuration.
-     *
-     * @return WebMvc configurer with CORS configured
-     */
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurerAdapter() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST","PUT", "DELETE");
-            }
-        };
     }
 
     @PostConstruct
--- src/main/java/com/withergate/api/config/ResourceServerConfig.java
+++ src/main/java/com/withergate/api/config/ResourceServerConfig.java
@@ -1,9 +1,14 @@
 package com.withergate.api.config;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
  * ResourceServerConfig class.
@@ -14,9 +19,27 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.R
 @EnableResourceServer
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
+    @Value("${withergate.corsOrigin}")
+    private String allowedOrigin;
+
     @Override
     public void configure(HttpSecurity http) throws Exception {
         http.cors().and().csrf().disable().authorizeRequests().anyRequest().authenticated();
+    }
+
+    /**
+     * CORS configuration.
+     *
+     * @return WebMvc configurer with CORS configured
+     */
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins(allowedOrigin).allowedMethods("GET", "POST","PUT", "DELETE");
+            }
+        };
     }
 
 }
