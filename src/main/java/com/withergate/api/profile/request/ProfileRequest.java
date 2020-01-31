434afcdd9200fb432d073a1bdbe98b7ec95a94ce
Martin Myslik <m.myslik@gmail.com>
2020-01-31 10:04:09 +0100

    task/#194 Implement profile creation

---
+++ src/main/java/com/withergate/api/profile/request/ProfileRequest.java
@@ -0,0 +1,17 @@
+package com.withergate.api.profile.request;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * Profile request. Used when creating new player profile.
+ *
+ * @author Martin Myslik
+ */
+@Getter
+@Setter
+public class ProfileRequest {
+
+    private String name;
+
+}
---
+++ src/main/java/com/withergate/api/service/profile/ProfileService.java
@@ -0,0 +1,32 @@
+package com.withergate.api.service.profile;
+
+import com.withergate.api.profile.model.Profile;
+import com.withergate.api.profile.request.ProfileRequest;
+import com.withergate.api.service.exception.EntityConflictException;
+import com.withergate.api.service.exception.ValidationException;
+
+/**
+ * Profile service.
+ *
+ * @author Martin Myslik
+ */
+public interface ProfileService {
+
+    /**
+     * Returns profile for given player ID.
+     *
+     * @param profileId player ID
+     * @return profile
+     */
+    Profile getProfile(int profileId);
+
+    /**
+     * Creates new player profile. Throws exception if not valid.
+     *
+     * @param request profile request
+     * @param playerId player ID
+     * @return created player profile
+     */
+    Profile createProfile(ProfileRequest request, int playerId) throws EntityConflictException, ValidationException;
+
+}
